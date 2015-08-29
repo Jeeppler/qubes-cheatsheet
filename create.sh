3d5a470115fe710fcbce3454c12b5241a174835e
Jeepler <jeremias.eppler@mailbox.org>
2015-08-29 23:16:39 +0200

    create.sh html output and comments added

--- create.sh
+++ create.sh
@@ -1,10 +1,15 @@
 #! /usr/bin/env bash
 
+# check if pandoc is installed
 if [[ `command -v pandoc` ]]
 then
+  # outputs the qubes cheat sheet as PDF file
   pandoc -s -o qubes-cheatsheet.pdf qubes-cheatsheet.md 
+
+  # outputs the qubes cheat sheet as HTML file
   pandoc -s -o qubes-cheatsheet.html qubes-cheatsheet.md
 else 
+  # pandoc is not installed inform the user
   echo "The command pandoc could not be found in the PATH!\n"
 fi
 
